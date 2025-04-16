37293b32fe6020d32042cb268ca4d5ed89b8b526
Hidayattt24 <hidayat.22@mhs.usk.ac.id>
2025-04-16 14:16:22 +0700

    refactor: restructure webpack configuration for clarity and consistency

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,20 +1,24 @@
-const { merge } = require("webpack-merge");
-const common = require("./webpack.common.js");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = merge(common, {
-  mode: "production",
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true, // optional: untuk membersihkan isi folder dist setiap build
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-          },
-        },
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
-});
+};
